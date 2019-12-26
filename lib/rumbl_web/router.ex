b72a8c9ffabafdc44a40b49fb4e2f003795f0e29
Andy Park <atpark333@gmail.com>
2019-12-26 13:24:00 +0900

    Add session resource routes

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -21,6 +21,7 @@ defmodule RumblWeb.Router do
     # get "/users/:id", UserController, :show
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
