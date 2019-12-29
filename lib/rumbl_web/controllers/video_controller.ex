00c7bcd645fa9f46c2371a7af2c896fe13a1ada8
Andy Park <atpark333@gmail.com>
2019-12-29 21:26:30 +0900

    Add functions to query videos by user

--- lib/rumbl_web/controllers/video_controller.ex
+++ lib/rumbl_web/controllers/video_controller.ex
@@ -3,19 +3,24 @@ defmodule RumblWeb.VideoController do
 
   alias Rumbl.Multimedia
   alias Rumbl.Multimedia.Video
+  
+  def action(conn, _params) do
+    args = [conn, conn.params, conn.assigns.current_user]
+    apply(__MODULE__, action_name(conn), args)
+  end
 
   def index(conn, _params) do
     videos = Multimedia.list_videos()
     render(conn, "index.html", videos: videos)
   end
 
-  def new(conn, _params) do
+  def new(conn, _params, _current_user) do
     changeset = Multimedia.change_video(%Video{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"video" => video_params}) do
-    case Multimedia.create_video(video_params) do
+  def create(conn, %{"video" => video_params}, current_user) do
+    case Multimedia.create_video(current_user, video_params) do
       {:ok, video} ->
         conn
         |> put_flash(:info, "Video created successfully.")
