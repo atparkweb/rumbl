cee797cef41769b9c79b3b2a8257688e97056a74
Andy Park <atpark333@gmail.com>
2020-01-01 15:27:44 +0900

    Update interface with video categories

--- lib/rumbl_web/controllers/video_controller.ex
+++ lib/rumbl_web/controllers/video_controller.ex
@@ -4,6 +4,12 @@ defmodule RumblWeb.VideoController do
   alias Rumbl.Multimedia
   alias Rumbl.Multimedia.Video
   
+  plug :load_categories when action in [:new, :create, :edit, :update]
+  
+  defp load_categories(conn, _) do
+    assign(conn, :categories, Multimedia.list_alphabetical_categories())
+  end
+  
   def action(conn, _params) do
     args = [conn, conn.params, conn.assigns.current_user]
     apply(__MODULE__, action_name(conn), args)
