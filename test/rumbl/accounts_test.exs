6c7f89623f39324f157cfccd0738a97764342d0c
Andy Park <atpark333@gmail.com>
2020-01-03 13:13:47 +0900

    Add tests fro user authentication

--- test/rumbl/accounts_test.exs
+++ test/rumbl/accounts_test.exs
@@ -46,4 +46,25 @@ defmodule Rumbl.AccountsTest do
     assert %{password: ["should be at least 6 character(s)"]} = errors_on(changeset)
     assert Accounts.list_users() == []
   end
+  
+  describe "authenticate_by_username_and_pass/2" do
+    @pass "123456"
+
+    setup do
+      {:ok, user: user_fixture(password: @pass)}
+    end
+    
+    test "returns user with correct password", %{user: user} do
+      assert {:ok, auth_user} = Accounts.authenticate_by_username_and_pass(user.username, @pass)
+      assert auth_user.id == user.id
+    end
+    
+    test "returns unauthorized error with invalid password", %{user: user} do
+      assert {:error, :unauthorized} = Accounts.authenticate_by_username_and_pass(user.username, "badpass")
+    end
+    
+    test "returns not found error with no matching user for email" do
+      assert {:error, :not_found} = Accounts.authenticate_by_username_and_pass("unknownuser", @pass)
+    end
+  end
 end
