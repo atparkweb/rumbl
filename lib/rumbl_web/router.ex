44b846f95e0c32c4f54f3c9eb46a9efb753d92d4
Andy Park <atpark333@gmail.com>
2020-01-06 13:46:30 +0900

    Add route for watching videos

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -22,6 +22,7 @@ defmodule RumblWeb.Router do
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    get "/watch/:id", WatchController, :show
   end
   
   scope "/manage", RumblWeb do
