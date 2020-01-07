f59eafd3598b78abf4fd3d923a95074ab469e8b5
Andy Park <atpark333@gmail.com>
2020-01-07 17:52:30 +0900

    Add primary key definition

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -1,6 +1,8 @@
 defmodule Rumbl.Multimedia.Video do
   use Ecto.Schema
   import Ecto.Changeset
+  
+  @primary_key {:id, Rumbl.Multimedia.Permalink, autogenerate: true}
 
   schema "videos" do
     field :description, :string
