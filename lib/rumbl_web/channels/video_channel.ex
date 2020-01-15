eda2f5af2f2670917becc44def2c258e39b36b73
Andy Park <atpark333@gmail.com>
2020-01-15 13:37:09 +0900

    Implement basic video channel pubsub

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -2,15 +2,15 @@ defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
   
   def join("videos:" <> video_id, _params, socket) do
-    :timer.send_interval(5_000, :ping)
-    # {:ok, assign(socket, :video_id, String.to_integer(video_id))}
     {:ok, socket}
   end
   
-  def handle_info(:ping, socket) do
-    count = socket.assigns[:count] || 1
-    push(socket, "ping", %{count: count})
-    
-    {:noreply, assign(socket, :count, count + 1)}
+  def handle_in("new_annotation", params, socket) do
+    broadcast!(socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    })
+    {:reply, :ok, socket}
   end
 end
