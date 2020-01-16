9dfd560f6e29cb12b519493648c18abb30ac214c
Andy Park <atpark333@gmail.com>
2020-01-16 16:53:43 +0900

    Add persisted annotations to videos

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -7,8 +7,11 @@ defmodule Rumbl.Multimedia do
   alias Rumbl.Repo
   alias Rumbl.Accounts
 
-  alias Rumbl.Multimedia.Video
-  alias Rumbl.Multimedia.Category
+  alias Rumbl.Multimedia.{
+    Video,
+    Category,
+    Annotation
+  }
 
   @doc """
   Returns the list of videos.
@@ -131,5 +134,20 @@ defmodule Rumbl.Multimedia do
     Category
     |> Category.alphabetical()
     |> Repo.all()
+  end
+  
+  def annotate_video(%Accounts.User{id: user_id}, video_id, attrs) do
+    %Annotation{video_id: video_id, user_id: user_id}
+    |> Annotation.changeset(attrs)
+    |> Repo.insert()
+  end
+  
+  def list_annotations(%Video{} = video) do
+    Repo.all(
+      from a in Ecto.assoc(video, :annotations),
+	order_by: [asc: a.at, asc: a.id],
+	limit: 500,
+	preload: [:user]
+    )
   end
 end
